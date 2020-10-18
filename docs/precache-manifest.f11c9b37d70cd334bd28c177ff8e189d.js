b290a7b4723dcf05388015ea35049a10759e6407
Angel Santillan <angelsb94@gmail.com>
2020-10-18 17:38:03 -0500

    add docs

---
+++ docs/precache-manifest.f11c9b37d70cd334bd28c177ff8e189d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700e3460d66de99f05f2fd12b9756951",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c883670371153b59eb94",
+    "url": "/static/css/main.c42807b5.chunk.css"
+  },
+  {
+    "revision": "6c1d956ed848b04fbeb2",
+    "url": "/static/js/2.ec823eca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ec823eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c883670371153b59eb94",
+    "url": "/static/js/main.4538cfab.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
