806fd0c86a30d3a0df418ac2ce7b5a58d2c47e53
Angel Santillan <angelsb94@gmail.com>
2020-10-18 14:04:49 -0500

    segunda clase del curso

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const GifExpertApp = () => {
-    return (
-        <div>
-            <h2>GifExpertApp</h2>
-            <hr/>
-        </div>
-    )
+
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+  //const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
+  const handleAdd = () => {
+    const newSerie = 'HunterX';
+    // setCategories([ newSerie, ...categories ]);
+    setCategories( cats => [ newSerie, ...cats ]);
+  }
+
+  return (
+    <div>
+      <h2>GifExpertApp</h2>
+      <hr/>
+      <button onClick={handleAdd}>Agregar</button>
+      <ol>
+        {
+          categories.map( category => {
+            return <li key={ category }> { category }</li>
+          })
+        }
+      </ol>
+    </div>
+  );
 }
 
 export default GifExpertApp
