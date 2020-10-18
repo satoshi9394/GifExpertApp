f4639a389d4614b01226850ce6664a120d207a87
Angel Santillan <angelsb94@gmail.com>
2020-10-18 16:22:31 -0500

    6er clase

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import GifGridItem from './GifGridItem';
 
 const GifGrid = ({category}) => {
+
+  const [images, setImages] = useState([])
+
+  useEffect( () => {
+    getGifs();
+  }, [])
 
   const getGifs =async () => {
     const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=RLnaxIFpwB9iUhmoXZUq1ln0ZjRWPAX9';
@@ -14,13 +21,20 @@ const GifGrid = ({category}) => {
       }
     })
     console.log(gifs);
+    setImages(gifs)
   }
-
-  getGifs();
 
   return (
     <div>
       <h3>{category}</h3>
+      {
+        images.map( img => (
+          <GifGridItem 
+            key={img.id}
+            { ...img }
+          />
+        ))
+      }
     </div>
   )
 }
